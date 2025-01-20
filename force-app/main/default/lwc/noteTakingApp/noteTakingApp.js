fccf8f1f5f5aab63dde4db1dca2bc9e51cabb635
Shubham Rawat <rawat.shubham2400@gmail.com>
2025-01-20 22:34:00 +0530

    Added some changes in the note cards styling, also incorporated the child component(Modal) into the Note Taking App LWC

--- force-app/main/default/lwc/noteTakingApp/noteTakingApp.js
+++ force-app/main/default/lwc/noteTakingApp/noteTakingApp.js
@@ -1,3 +1,50 @@
+import Description from '@salesforce/schema/Account.Description';
 import { LightningElement } from 'lwc';
 
-export default class NoteTakingApp extends LightningElement {}
+const DEFAULT_NOTE_FORM = {
+    Name: "",
+    Note_Description__c: ""
+}
+
+export default class NoteTakingApp extends LightningElement {
+    showModal = false;
+    noteRecord = DEFAULT_NOTE_FORM;
+    formats = [
+        'font',
+        'size',
+        'bold',
+        'italic',
+        'underline',
+        'strike',
+        'list',
+        'indent',
+        'align',
+        'link',
+        'clean',
+        'table',
+        'header',
+        'color',
+    ];
+
+    get isFormInvalid(){
+        return !(this.noteRecord && this.noteRecord.Note_Description__c && this.noteRecord.Name);
+    }
+
+    createNoteHandler(){
+        this.showModal = true;
+        this.noteRecord = DEFAULT_NOTE_FORM;
+    }
+
+    closeModalHandler(){
+        this.showModal = false;
+    }
+
+    changeHandler(event){
+        const {name, value} = event.target;
+        this.noteRecord = {...this.noteRecord, [name]: value};
+    }
+
+    formSubmitHandler(event){
+        event.preventDefault();
+    }
+}

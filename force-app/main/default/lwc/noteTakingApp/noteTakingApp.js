703ba4b0a32648df4444b9c23533832e35639138
Shubham Rawat <rawat.shubham2400@gmail.com>
2025-01-22 10:02:13 +0530

    Incorporated the editing and updating the Notes functionality in the NoteTakingApp LWC.

--- force-app/main/default/lwc/noteTakingApp/noteTakingApp.js
+++ force-app/main/default/lwc/noteTakingApp/noteTakingApp.js
@@ -1,6 +1,8 @@
 import { LightningElement, wire } from 'lwc';
 import createNoteRecord from '@salesforce/apex/NoteTakingHandler.createNoteRecord';
 import getNotes from '@salesforce/apex/NoteTakingHandler.getNotes';
+import updateNoteRecord from '@salesforce/apex/NoteTakingHandler.updateNoteRecord';
+
 const DEFAULT_NOTE_FORM = {
     Name: "",
     Note_Description__c: ""
@@ -9,6 +11,7 @@ const DEFAULT_NOTE_FORM = {
 export default class NoteTakingApp extends LightningElement {
     showModal = false;
     noteRecord = DEFAULT_NOTE_FORM;
+    selectedRecordId = '';
     noteList = [];
     formats = [
         'font',
@@ -27,55 +30,89 @@ export default class NoteTakingApp extends LightningElement {
         'color',
     ];
 
-    get isFormInvalid(){
+    get isFormInvalid() {
         return !(this.noteRecord && this.noteRecord.Note_Description__c && this.noteRecord.Name);
     }
 
-    createNoteHandler(){
+    get ModalName() {
+        return this.selectedRecordId ? "Update Note" : "Add Note";
+    }
+
+    createNoteHandler() {
         this.showModal = true;
         this.noteRecord = DEFAULT_NOTE_FORM;
     }
 
-    closeModalHandler(){
+    closeModalHandler() {
         this.showModal = false;
+        this.noteRecord = DEFAULT_NOTE_FORM;
+        this.selectedRecordId = null;
     }
 
-    changeHandler(event){
-        const {name, value} = event.target;
-        this.noteRecord = {...this.noteRecord, [name]: value};
+    changeHandler(event) {
+        const { name, value } = event.target;
+        this.noteRecord = { ...this.noteRecord, [name]: value };
     }
 
-    formSubmitHandler(event){
+    formSubmitHandler(event) {
         event.preventDefault();
-        this.createNote();
+
+        
+        if(this.selectedRecordId){
+            this.updateNote(this.selectedRecordId);
+        }else{
+            this.createNote();
+        }
     }
 
-    createNote(){
-        createNoteRecord({title: this.noteRecord.Name, description: this.noteRecord.Note_Description__c}).then(()=>{
+    createNote() {
+        createNoteRecord({ title: this.noteRecord.Name, description: this.noteRecord.Note_Description__c }).then(() => {
             this.showModal = false;
             this.showToastMsg("Note Created Successfully!!", "success");
-        }).catch(error=> {
+        }).catch(error => {
             this.showToastMsg(error.message.body, "error");
         });
     }
 
-    showToastMsg(message, variant){
+    showToastMsg(message, variant) {
         const elem = this.template.querySelector('c-notifiction');
-        if(elem){
+        if (elem) {
             elem.showToast(message, variant);
         }
     }
 
     @wire(getNotes)
-    noteListInfo({data, error}){
-        if(data){
+    noteListInfo({ data, error }) {
+        if (data) {
             this.noteList = data.map(item => {
                 let formattedDate = new Date(item.LastModifiedDate).toDateString();
-                return {...item, formattedDate};
+                return { ...item, formattedDate };
             })
         }
-        if(error){
+        if (error) {
             this.showToastMsg(error.message.body, 'error');
         }
+    }
+
+    editNoteHandler(event) {
+        const { recordid } = event.target.dataset;
+        const noteRecord = this.noteList.find(item => item.Id === recordId);
+        this.noteRecord = {
+            Name: noteRecord.Name,
+            Note_Description__c: noteRecord.Note_Description__c
+        }
+        this.selectedRecordId = recordid;
+        this.showModal = true;
+    }
+
+    updateNote(noteId) {
+        const [Name, Note_Description__c] = this.noteRecord;
+        updateNoteRecord({ noteId, title: Name, description: Note_Description__c }).then(() => {
+            this.showModal = false;
+            this.showToastMsg("Note Updated Successfully!!", 'success');
+        }).catch(error => {
+            console.error("Error in Updating", error);
+            this.showToastMsg(error.message.body, 'error');
+        })
     }
 }

7c3d050ed27d7be4ae2243f314f63e6a0c781f4f
Shubham Rawat <rawat.shubham2400@gmail.com>
2025-01-26 19:59:57 +0530

    Created the css file for our user details and stats component as well as incorporated the user stats and user details components within the wrapper component.

--- force-app/main/default/lwc/portfolioUserDetailsAndStatsWrapper/portfolioUserDetailsAndStatsWrapper.js
+++ force-app/main/default/lwc/portfolioUserDetailsAndStatsWrapper/portfolioUserDetailsAndStatsWrapper.js
@@ -1,3 +1,11 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class PortfolioUserDetailsAndStatsWrapper extends LightningElement {}
+export default class PortfolioUserDetailsAndStatsWrapper extends LightningElement {
+    @api recordId;
+    @api objectApiName;
+    @api rank;
+    @api badges;
+    @api points;
+    @api trails;
+    @api resumeUrl;
+}

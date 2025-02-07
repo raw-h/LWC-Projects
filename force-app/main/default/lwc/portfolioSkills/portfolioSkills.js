088a38226338cef6cfec7ad9794a38be0d1a7537
Shubham Rawat <rawat.shubham2400@gmail.com>
2025-02-07 23:45:49 +0530

    Worked on the portfolioSkills component.

--- force-app/main/default/lwc/portfolioSkills/portfolioSkills.js
+++ force-app/main/default/lwc/portfolioSkills/portfolioSkills.js
@@ -1,5 +1,21 @@
 import { LightningElement, wire, api } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
-export default class PortfolioSkills extends LightningElement { 
+import Tech_Skills_Field from '@salesforce/schema/Portfolio__c.TechnicalSkills__c';
+import Software_Field from '@salesforce/schema/Portfolio__c.SoftwareTools__c';
+import Methodologies_Field from '@salesforce/schema/Portfolio__c.SoftwareDevelopmentMethodologies__c';
+import Soft_Skills_Field from '@salesforce/schema/Portfolio__c.SoftSkills__c';
+
+export default class PortfolioSkills extends LightningElement {
     @api recordId;
+    @wire(getRecord, {
+        recordId: '$recordId',
+        fields: [Tech_Skills_Field, Soft_Skills_Field, Software_Field, Methodologies_Field]
+    })skillHandler({data, error}){
+        if(data){
+
+        }
+        if(error){
+            
+        }
+    }
 }

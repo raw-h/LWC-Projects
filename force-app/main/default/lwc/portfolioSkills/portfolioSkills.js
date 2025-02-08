ff8cd1769a9e8b3b2ba7a75efc694fc9bd56e09f
Shubham Rawat <rawat.shubham2400@gmail.com>
2025-02-08 11:57:26 +0530

    Created the css file for skills component and completed the whole component.

--- force-app/main/default/lwc/portfolioSkills/portfolioSkills.js
+++ force-app/main/default/lwc/portfolioSkills/portfolioSkills.js
@@ -6,16 +6,29 @@ import Methodologies_Field from '@salesforce/schema/Portfolio__c.SoftwareDevelop
 import Soft_Skills_Field from '@salesforce/schema/Portfolio__c.SoftSkills__c';
 
 export default class PortfolioSkills extends LightningElement {
+    techSkills = [];
+    softSkills = [];
+    methodologies = [];
+    toolsSkills = [];
+
     @api recordId;
     @wire(getRecord, {
         recordId: '$recordId',
         fields: [Tech_Skills_Field, Soft_Skills_Field, Software_Field, Methodologies_Field]
-    })skillHandler({data, error}){
-        if(data){
+    }) skillHandler({ data, error }) {
+        if (data) {
+            this.formatSkills(data);
+        }
+        if (error) {
 
         }
-        if(error){
-            
-        }
+    }
+
+    formatSkills(data) {
+        const { SoftSkills__c, SoftwareDevelopmentMethodologies__c, SoftwareTools__c, TechnicalSkills__c } = data.fields;
+        this.techSkills = TechnicalSkills__c ? TechnicalSkills__c.value.split(',') : [];
+        this.softSkills = SoftSkills__c ? SoftSkills__c.value.split(',') : [];
+        this.methodologies = SoftwareDevelopmentMethodologies__c ? SoftwareDevelopmentMethodologies__c.value.split(',') : [];
+        this.toolsSkills = SoftwareTools__c ? SoftwareTools__c.value.split(',') : [];
     }
 }

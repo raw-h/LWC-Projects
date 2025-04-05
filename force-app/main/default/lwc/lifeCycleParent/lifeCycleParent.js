c132fca2bee75220e4a55fef928789e2055fc1d8
Shubham Rawat <rawat.shubham2400@gmail.com>
2025-04-05 14:46:08 +0530

    Error callback has been added in the lifeCycleParent component.

--- force-app/main/default/lwc/lifeCycleParent/lifeCycleParent.js
+++ force-app/main/default/lwc/lifeCycleParent/lifeCycleParent.js
@@ -1,11 +1,25 @@
 import { LightningElement } from 'lwc';
 
 export default class LifeCycleParent extends LightningElement {
+    isChildVisible = false;
+
     constructor(){
         super();
         console.log("Parent constructor called.");
     }
     connectedCallback(){
         console.log("Parent connectedCallback Called.");
+    }
+    renderedCallback(){
+        console.log("parent rendered Callback called.");
+    }
+
+    handleClick(){
+        this.isChildVisible = !this.isChildVisible;
+    }
+
+    errorCallback(error, stack){
+        console.log(error.message);
+        console.log(stack);
     }
 }

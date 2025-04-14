f7d7ffa4aac61dbbbddc2df459b33dda414d7250
Shubham Rawat <rawat.shubham2400@gmail.com>
2025-04-14 22:59:13 +0530

    Event bubbling covered, with c2pModalComponent and c2pModalComponent.

--- force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
+++ force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
@@ -3,8 +3,13 @@ import { LightningElement } from 'lwc';
 export default class C2pModalComponent extends LightningElement {
     closeHandler(){
         const closeEvent = new CustomEvent('close', {
+            bubbles: true,
             detail : "Modal closed successfully"
         });
         this.dispatchEvent(closeEvent);
+    }
+
+    footerHandler(){
+        console.log("Footer Handler called");
     }
 }

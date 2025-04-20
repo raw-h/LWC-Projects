2f82070985350b481343791a15b3e590143cb4b0
Shubham Rawat <rawat.shubham2400@gmail.com>
2025-04-20 17:22:01 +0530

    Setter Demo Parent Component.

---
+++ force-app/main/default/lwc/setterDemoParent/setterDemoParent.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class SetterDemoParent extends LightningElement {
+    userDetails = {
+        name: "Shubham Singh Rawat",
+        age: 25
+    }
+}

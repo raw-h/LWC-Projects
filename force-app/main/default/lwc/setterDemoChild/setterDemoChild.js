c8323cc8ed6e3c64bf6aca4e794a4d6487865f5e
Shubham Rawat <rawat.shubham2400@gmail.com>
2025-04-20 17:22:32 +0530

    Setter Demo Child Component.

---
+++ force-app/main/default/lwc/setterDemoChild/setterDemoChild.js
@@ -0,0 +1,15 @@
+import { LightningElement, api } from 'lwc';
+
+export default class SetterDemoChild extends LightningElement {
+    userDetail;
+    
+    @api 
+    get detail(){
+        return this.userDetail;
+    }
+
+    set detail(data){
+        let newAge = data.age * 2;
+        this.userDetail = {...data, age: newAge, "location" : "Noida"};
+    }
+}
